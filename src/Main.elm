a98e42d51f3733412c53513bf5686d52792f32fb
Corey Haines <coreyhaines@gmail.com>
2020-08-22 09:52:55 -0500

    Basic elm-ui skeleton w/ hardcoded values

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,43 @@
 module Main exposing (main)
 
+{- Combine GPX Files From Local System
+   Tasks Remaining
+   [ ] Set up Layout
+
+   [ ] Add Files
+   [ ] Remove Files
+   [ ] Reorder Files
+
+   [ ] Parse Files (xml)
+   [ ] Show Parsed Information
+
+
+   [ ] Export Combined File
+-}
+
 import Browser
-import Html exposing (h1, text)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html)
 import Html.Attributes exposing (style)
 
 
 
 -- MODEL
+
+
+type alias Files =
+    List String
+
+
+loadedFiles : Files
+loadedFiles =
+    [ "Monday, August 24 12:30pm"
+    , "Monday, August 24 1:15pm"
+    ]
 
 
 type alias Model =
@@ -31,13 +62,82 @@ init _ =
 
 
 view : Model -> Browser.Document Message
-view model =
+view _ =
     { title = "Combine GPX Files"
     , body =
-        [ h1 [ style "display" "flex", style "justify-content" "center" ]
-            [ text "Combine GPX Files" ]
+        [ bodyView
         ]
     }
+
+
+buttonView : String -> Element Message
+buttonView label =
+    Input.button
+        [ Background.color (rgb255 0 0 238)
+        , Border.width 1
+        , Border.rounded 10
+        , padding 5
+        , centerX
+        ]
+        { onPress = Nothing
+        , label =
+            el [ Font.color (Element.rgb255 255 255 255) ] <| text label
+        }
+
+
+menuView : Element Message
+menuView =
+    column
+        [ height fill
+        , Border.color (rgb 0 0 0)
+        , Border.width 1
+        , padding 20
+        , spacing 10
+        ]
+        [ text "Things We Can Do"
+        , buttonView "Load Files"
+        , buttonView "Export File"
+        ]
+
+
+filesView : Files -> Element Message
+filesView files =
+    column [ spacing 10 ] <| List.map text files
+
+
+fileListView : Element Message
+fileListView =
+    column
+        [ height fill
+        , width fill
+        , Border.color (rgb 0 0 0)
+        , Border.width 1
+        , padding 20
+        , spacing 40
+        ]
+        [ el [ centerX ] <| text "Loaded Files"
+        , filesView loadedFiles
+        ]
+
+
+menu : Element Message
+menu =
+    row
+        [ width fill
+        , padding 20
+        , Border.color (rgb 0 0 0)
+        , Border.width 1
+        ]
+        [ el [ centerX ] <| text "Combine GPX Files" ]
+
+
+bodyView : Html Message
+bodyView =
+    layout [] <|
+        column [ height fill, width fill ] <|
+            [ menu
+            , row [ height fill, width fill ] [ menuView, fileListView ]
+            ]
 
 
 
