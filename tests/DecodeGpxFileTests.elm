3cb4fd5f3e2990f5834d9318b742042811cb00ee
Corey Haines <coreyhaines@gmail.com>
2020-08-24 14:43:31 -0500

    Decode track points for a full gpx document

--- tests/DecodeGpxFileTests.elm
+++ tests/DecodeGpxFileTests.elm
@@ -14,6 +14,11 @@ type alias TrackPoint =
     }
 
 
+type alias GpxFile =
+    { trackPoints : List TrackPoint
+    }
+
+
 trkptLineDecoder : XmlDecode.Decoder TrackPoint
 trkptLineDecoder =
     XmlDecode.map4 TrackPoint
@@ -28,11 +33,30 @@ trksegDecoder =
     XmlDecode.path [ "trkpt" ] <| XmlDecode.list trkptLineDecoder
 
 
+gpxDecoder : XmlDecode.Decoder GpxFile
+gpxDecoder =
+    XmlDecode.map GpxFile
+        (XmlDecode.path [ "trk", "trkseg" ] <| XmlDecode.single trksegDecoder)
+
+
 suite : Test
 suite =
     describe "Decoding a gpx file's xml"
         [ testDecodingTrackPointLine
         , testDecodingTrackSegmentLine
+        , testDecodingFullGpx
+        ]
+
+
+testDecodingFullGpx : Test
+testDecodingFullGpx =
+    describe "Decoding the full gpx document"
+        [ test "Decoding track segments" <|
+            \_ ->
+                XmlDecode.decodeString gpxDecoder exampleGpx
+                    |> Result.map (.trackPoints >> List.length)
+                    |> Result.withDefault -1
+                    |> Expect.equal 5
         ]
 
 
@@ -93,3 +117,28 @@ trksegLine =
     <trkpt lat="42.007962000" lon="-87.665366000"><ele>185.0</ele><time>2020-08-21T19:53:25Z</time></trkpt>
 </trkseg>
 """
+
+
+exampleGpx : String
+exampleGpx =
+    """
+<gpx
+  version="1.1"
+  creator="Runkeeper - http://www.runkeeper.com"
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xmlns="http://www.topografix.com/GPX/1/1"
+  xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"
+  xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1">
+    <trk>
+        <name><![CDATA[Cycling 8/21/20 2:53 pm]]></name>
+        <time>2020-08-21T19:53:12Z</time>
+        <trkseg>
+            <trkpt lat="42.007962000" lon="-87.665366000"><ele>185.0</ele><time>2020-08-21T19:53:12Z</time></trkpt>
+            <trkpt lat="42.007962000" lon="-87.665366000"><ele>185.0</ele><time>2020-08-21T19:53:25Z</time></trkpt>
+            <trkpt lat="42.007944000" lon="-87.665227000"><ele>185.0</ele><time>2020-08-21T19:53:28Z</time></trkpt>
+            <trkpt lat="42.007901000" lon="-87.665130000"><ele>185.0</ele><time>2020-08-21T19:53:30Z</time></trkpt>
+            <trkpt lat="42.007837000" lon="-87.665056000"><ele>185.0</ele><time>2020-08-21T19:53:32Z</time></trkpt>
+        </trkseg>
+    </trk>
+</gpx>
+    """
