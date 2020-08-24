87053a8ca693e8929a588dba7dbbc4b0d41744f8
Corey Haines <coreyhaines@gmail.com>
2020-08-24 14:49:29 -0500

    Extract GpxFile module from test file

--- tests/DecodeGpxFileTests.elm
+++ tests/DecodeGpxFileTests.elm
@@ -2,45 +2,9 @@ module DecodeGpxFileTests exposing (suite)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import GpxFile exposing (GpxFile, TrackPoint, gpxDecoder, trkptLineDecoder, trksegDecoder)
 import Test exposing (..)
 import Xml.Decode as XmlDecode
-
-
-type alias TrackPoint =
-    { elevation : String
-    , time : String
-    , latitude : String
-    , longitude : String
-    }
-
-
-type alias GpxFile =
-    { name : String
-    , time : String
-    , trackPoints : List TrackPoint
-    }
-
-
-trkptLineDecoder : XmlDecode.Decoder TrackPoint
-trkptLineDecoder =
-    XmlDecode.map4 TrackPoint
-        (XmlDecode.path [ "ele" ] <| XmlDecode.single XmlDecode.string)
-        (XmlDecode.path [ "time" ] <| XmlDecode.single XmlDecode.string)
-        (XmlDecode.stringAttr "lat")
-        (XmlDecode.stringAttr "lon")
-
-
-trksegDecoder : XmlDecode.Decoder (List TrackPoint)
-trksegDecoder =
-    XmlDecode.path [ "trkpt" ] <| XmlDecode.list trkptLineDecoder
-
-
-gpxDecoder : XmlDecode.Decoder GpxFile
-gpxDecoder =
-    XmlDecode.map3 GpxFile
-        (XmlDecode.path [ "trk", "name" ] <| XmlDecode.single XmlDecode.string)
-        (XmlDecode.path [ "trk", "time" ] <| XmlDecode.single XmlDecode.string)
-        (XmlDecode.path [ "trk", "trkseg" ] <| XmlDecode.single trksegDecoder)
 
 
 suite : Test
