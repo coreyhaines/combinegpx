1b679fd1c1f54b4ca7fdc6544faf74fc055afc63
Corey Haines <coreyhaines@gmail.com>
2020-08-24 14:26:27 -0500

    Learn how to decode xml attribute

--- tests/DecodeGpxFileTests.elm
+++ tests/DecodeGpxFileTests.elm
@@ -7,14 +7,20 @@ import Xml.Decode as XmlDecode
 
 
 type alias TrackPoint =
-    { time : String
+    { elevation : String
+    , time : String
+    , latitude : String
+    , longitude : String
     }
 
 
 trkptLineDecoder : XmlDecode.Decoder TrackPoint
 trkptLineDecoder =
-    XmlDecode.map TrackPoint
+    XmlDecode.map4 TrackPoint
+        (XmlDecode.path [ "ele" ] <| XmlDecode.single XmlDecode.string)
         (XmlDecode.path [ "time" ] <| XmlDecode.single XmlDecode.string)
+        (XmlDecode.stringAttr "lat")
+        (XmlDecode.stringAttr "lon")
 
 
 suite : Test
@@ -26,12 +32,32 @@ suite =
 
 testDecodingTrackPointLine : Test
 testDecodingTrackPointLine =
-    test "Decoding trkpt node" <|
-        \_ ->
-            XmlDecode.decodeString trkptLineDecoder trkptLine
-                |> Result.map .time
-                |> Result.withDefault ""
-                |> Expect.equal "2020-08-21T19:53:12Z"
+    describe "Decoding trkpt node"
+        [ test "Decoding time" <|
+            \_ ->
+                XmlDecode.decodeString trkptLineDecoder trkptLine
+                    |> Result.map .time
+                    |> Result.withDefault ""
+                    |> Expect.equal "2020-08-21T19:53:12Z"
+        , test "Decoding elevation" <|
+            \_ ->
+                XmlDecode.decodeString trkptLineDecoder trkptLine
+                    |> Result.map .elevation
+                    |> Result.withDefault ""
+                    |> Expect.equal "185.0"
+        , test "Decoding latitude" <|
+            \_ ->
+                XmlDecode.decodeString trkptLineDecoder trkptLine
+                    |> Result.map .latitude
+                    |> Result.withDefault ""
+                    |> Expect.equal "42.007962000"
+        , test "Decoding longitude" <|
+            \_ ->
+                XmlDecode.decodeString trkptLineDecoder trkptLine
+                    |> Result.map .longitude
+                    |> Result.withDefault ""
+                    |> Expect.equal "-87.665366000"
+        ]
 
 
 trkptLine : String
