4c01d4a3d82719c1b3efd9b8e390e55fdc9a164e
Corey Haines <coreyhaines@gmail.com>
2020-08-25 14:57:12 -0500

    Reorganize order of functions in GpxFile to highlight usage

--- src/GpxFile.elm
+++ src/GpxFile.elm
@@ -24,13 +24,12 @@ parseGpxData =
         >> Result.toMaybe
 
 
-trkptLineDecoder : XmlDecode.Decoder TrackPoint
-trkptLineDecoder =
-    XmlDecode.map4 TrackPoint
-        (XmlDecode.path [ "ele" ] <| XmlDecode.single XmlDecode.string)
-        (XmlDecode.path [ "time" ] <| XmlDecode.single XmlDecode.string)
-        (XmlDecode.stringAttr "lat")
-        (XmlDecode.stringAttr "lon")
+gpxDecoder : XmlDecode.Decoder GpxFile
+gpxDecoder =
+    XmlDecode.map3 GpxFile
+        (XmlDecode.path [ "trk", "name" ] <| XmlDecode.single XmlDecode.string)
+        (XmlDecode.path [ "trk", "time" ] <| XmlDecode.single XmlDecode.string)
+        (XmlDecode.path [ "trk", "trkseg" ] <| XmlDecode.single trksegDecoder)
 
 
 trksegDecoder : XmlDecode.Decoder (List TrackPoint)
@@ -38,9 +37,10 @@ trksegDecoder =
     XmlDecode.path [ "trkpt" ] <| XmlDecode.list trkptLineDecoder
 
 
-gpxDecoder : XmlDecode.Decoder GpxFile
-gpxDecoder =
-    XmlDecode.map3 GpxFile
-        (XmlDecode.path [ "trk", "name" ] <| XmlDecode.single XmlDecode.string)
-        (XmlDecode.path [ "trk", "time" ] <| XmlDecode.single XmlDecode.string)
-        (XmlDecode.path [ "trk", "trkseg" ] <| XmlDecode.single trksegDecoder)
+trkptLineDecoder : XmlDecode.Decoder TrackPoint
+trkptLineDecoder =
+    XmlDecode.map4 TrackPoint
+        (XmlDecode.path [ "ele" ] <| XmlDecode.single XmlDecode.string)
+        (XmlDecode.path [ "time" ] <| XmlDecode.single XmlDecode.string)
+        (XmlDecode.stringAttr "lat")
+        (XmlDecode.stringAttr "lon")
